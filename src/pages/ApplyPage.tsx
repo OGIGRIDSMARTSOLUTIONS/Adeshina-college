18417ed80eef219fa1910666b026b4cba773be99
Oyeniran Samuel <oyeniransamuel2007@gmail.com>
2026-08-29 02:49:24 +0100

    latest changes

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { 
-  CheckCircle2, 
   Phone, 
   Mail, 
   Upload, 
@@ -12,7 +11,6 @@ import {
   ShieldCheck, 
   Printer, 
   Check,
-  Sparkles,
   X
 } from 'lucide-react';
 import { programmes } from '@/data/programmes';
